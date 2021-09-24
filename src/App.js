c6412aff68a63f7dde09d6553cd904638e84ae82
mrgomesg <mrgomesg@gmail.com>
2021-09-24 16:40:01 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React TESTE 7
         </a>
       </header>
     </div>
